3dd479268eb40451ea1bc6bcd1b1ed0f953d1f34
filipstrand <strand.filip@gmail.com>
2024-09-04 00:31:23 +0200

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mflux",
-    version="0.1.4",
+    version="0.1.5",
     author="Filip Strand",
     author_email="strand.filip@gmail.com",
     description="A MLX port of FLUX based on the Huggingface Diffusers implementation.",
